1162e19a557d4551ba804dd10854361a1a275977
gachu13 <hezekielg@gmail.com>
2018-10-29 13:41:49 +0300

    edit 1

--- resources/views/admin/layout/includes/sidenav.blade.php
+++ resources/views/admin/layout/includes/sidenav.blade.php
@@ -2,7 +2,8 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true" data-img="{{asset('theme-assets/images/backgrounds/02.jpg')}}">
   <div class="navbar-header">
     <ul class="nav navbar-nav flex-row">
-      <li class="nav-item mr-auto"><a class="navbar-brand" href="{{route('\')}}"><img class="brand-logo" alt="Chameleon admin logo" src="{{asset('theme-assets/images/logo/logo.png')}}"/>
+      <li class="nav-item mr-auto">
+        <a class="navbar-brand" href="{{url('admin')}}"><img class="brand-logo" alt="Chameleon admin logo" src="{{asset('theme-assets/images/logo/logo.png')}}"/>
           <h3 class="brand-text">Quick Link</h3></a></li>
       <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
     </ul>
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -4,14 +4,14 @@
 
       <!-- Slider -->
 
-      <div class="main_slider" style="background-image:url(images/slider_1.jpg)">
+      <div class="main_slider" style="background-image:url(images/advert_1.jpg); ">
         <div class="container fill_height">
           <div class="row align-items-center fill_height">
             <div class="col">
-              <div class="main_slider_content">
-                <h6>Spring / Summer Collection 2017</h6>
-                <h1>Get up to 30% Off New Arrivals</h1>
-                <div class="red_button shop_now_button"><a href="#">shop now</a></div>
+              <div class="main_slider_content" style="float: right;">
+                <h6 style="color: white;">Limited Edition 2018</h6><!-- description in application of advertisements 1 -->
+                <h1 style="color: white;">Limited stock</h1><!-- description in application of advertisements 2 -->
+                <div class="red_button shop_now_button"><a href="#" style="font-size: 18px;">shop now</a></div>
               </div>
             </div>
           </div>
@@ -23,24 +23,27 @@
       <div class="banner">
         <div class="container">
           <div class="row">
+            <!-- category 1 -->
             <div class="col-md-4">
-              <div class="banner_item align-items-center" style="background-image:url(images/banner_1.jpg)">
+              <div class="banner_item align-items-center" style="background-image:url(images/home_tools.jpg)">
                 <div class="banner_category">
-                  <a href="categories.html">women's</a>
+                  <a href="categories.html">Hardware</a>
                 </div>
               </div>
             </div>
+            <!-- category 2 -->
             <div class="col-md-4">
-              <div class="banner_item align-items-center" style="background-image:url(images/banner_2.jpg)">
+              <div class="banner_item align-items-center" style="background-image:url(images/Electricals.jpeg)">
                 <div class="banner_category">
-                  <a href="categories.html">accessories's</a>
+                  <a href="categories.html">Electricals</a>
                 </div>
               </div>
             </div>
+            <!-- category 3 -->
             <div class="col-md-4">
-              <div class="banner_item align-items-center" style="background-image:url(images/banner_3.jpg)">
+              <div class="banner_item align-items-center" style="background-image:url(images/electronics.jpg)">
                 <div class="banner_category">
-                  <a href="categories.html">men's</a>
+                  <a href="categories.html">Electronics</a>
                 </div>
               </div>
             </div>
@@ -115,7 +118,7 @@
                   <div class="row align-items-center">
                     <div class="col-lg-6">
                       <div class="deal_ofthe_week_img">
-                        <img src="{{asset('images/deal_ofthe_week.png')}}" alt="">
+                        <img src="{{asset('images/advert_3.png')}}" alt="">
                       </div>
                     </div>
                     <div class="col-lg-6 text-right deal_ofthe_week_col">
@@ -141,7 +144,7 @@
                             <div class="timer_unit">Sec</div>
                           </li>
                         </ul>
-                        <div class="red_button deal_ofthe_week_button"><a href="#">shop now</a></div>
+                        <div class="red_button shop_now_button"><a href="#">shop now</a></div>
                       </div>
                     </div>
                   </div>
