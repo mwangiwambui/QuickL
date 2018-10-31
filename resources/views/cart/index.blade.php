87fc617ee6798e739a78285c2359e9c38b4ac829
Wambui <wambui54mwangi@gmail.com>
2018-10-31 13:40:12 +0300

    edit 1

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -40,9 +40,10 @@
                     <th>Price/unit</th>
                     <th>Quantity</th>
                     <th>Size</th>
+                                    <th>contact industry</th>
   									<th>refresh</th>
   									<th>Delete</th>
-                    <th>contact industry</th>
+
   								</tr>
   							</thead>
   							<tbody>
@@ -58,6 +59,47 @@
                             <td>
                                 {!! Form::select('size', ['small'=>'Small','medium'=>'Medium','large'=>'Large'] , $cartItem->options->has('size')?$cartItem->options->size:'' ) !!}
                             </td>
+                                                        <td>
+                                                            <div class="red_buttons " onclick="openForm()" style="width: 200px;"><a href="#" style="font-size:15px;">Contact Industry</a></div>
+                                                            <div class="form-popup" id="myForm">
+                                                                <div class="super_container">
+                                                                    <div class="">
+                                                                        <div class="col-12">
+                                                                            <div class="card">
+                                                                                <div class="card-header">
+                                                                                    <h4 class="card-title">Industry Name</h4>
+                                                                                </div>
+                                                                                <div class="card-content collapse show">
+                                                                                    <div class="card-body">
+
+                                                                                            <div class="row">
+                                                                                                <div class="col-md-7">
+                                                                                                    <h3>{{$cartItem->name}}</h3>
+                                                                                                </div>
+                                                                                                <div class="col-md-5">
+                                                                                                    <input name="qty" type="text" class="form-control" style="width:100px;height:30px;" value="qty">
+                                                                                                </div>
+                                                                                            </div>
+
+                                                                                            <div class="control-group form-group">
+                                                                                                <div class="controls">
+                                                                                                    {{ Form::label('description', 'Message') }}
+                                                                                                    {{ Form::textarea('description', null, array('class' => 'form-control','id'=>'message', 'required' ,'data-validation-required-message'=>'Please enter your message')) }}
+
+                                                                                                </div>
+                                                                                            </div>
+
+                                                                                            <button type="submit" class="btn">Send Inquiry</button>
+                                                                                            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </td>
 
                             <td>
                               <button style="float: left" type="submit" class="btn btn-primary"><i class="fas fa-sync-alt"></i></button>
@@ -65,53 +107,15 @@
                               {{--<a class="button" href="{{route('cart.destroy',$cartItem->rowId}}"Delete--}}
                               {{--</a>--}}
                             </td>
-          									<td>
+
+                                                        <td>
                               <form action="{{route('cart.destroy',$cartItem->rowId)}}" method="POST">
                                   {{csrf_field()}}
                                   {{method_field('DELETE')}}
                                   <button class="btn btn-danger  btn-glow  mx-1" type="submit"><i class="far fa-trash-alt"></i></button>
                               </form>
                             </td>
-                            <td>
-                              <div class="red_buttons " onclick="openForm()" style="width: 200px;"><a href="#" style="font-size:15px;">Contact Industry</a></div>
-                              <div class="form-popup" id="myForm">
-                                <div class="super_container">
-                                  <div class="">
-                                    <div class="col-12">
-                                      <div class="card">
-                                        <div class="card-header">
-                                          <h4 class="card-title">Industry Name</h4>
-                                        </div>
-                                        <div class="card-content collapse show">
-                                          <div class="card-body">
-                                            <form action="/action_page.php" class="form-container">
-                                              <div class="row">
-                                                <div class="col-md-7">
-                                                  <h3>{{$cartItem->name}}</h3>
-                                                </div>
-                                                <div class="col-md-5">
-                                                  <input name="qty" type="text" class="form-control" style="width:100px;height:30px;" value="qty">
-                                                </div>
-                                              </div>
 
-                                              <div class="control-group form-group">
-                                                  <div class="controls">
-                                                      <label for="message">Message:</label>
-                                                      <textarea class="form-control" id="message" required data-validation-required-message="Please enter your message" ></textarea>
-                                                  </div>
-                                              </div>
-
-                                              <button type="submit" class="btn">Send Inquiry</button>
-                                              <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-                                            </form>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </td>
   													</tr>
                       @endforeach
   							</tbody>
